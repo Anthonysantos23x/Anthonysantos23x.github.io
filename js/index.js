89cf52d6952d870abb18d262cc29e8ac4cf3dbf5
Anthonysantos23x <111996861+Anthonysantos23x@users.noreply.github.com>
2022-11-10 09:28:01 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let cadenastart = "trabaja duro en silencio"
+let cadenaEnd = "y tu exito hara todo el ruido"
+let finalcadena= cadenastart + cadenaEnd
+alert(finalcadena)
+let cadenalength= finalcadena.length
+console.log(cadenalength)
+alert(cadenalength)
